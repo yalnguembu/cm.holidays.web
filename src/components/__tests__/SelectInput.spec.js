0d0d7d8ba5687a94069cd60d93fc5202fcd14c90
yalnguembu <yalnguembu@gmail.com>
2023-01-25 16:00:19 +0100

    remove commented code

--- src/components/__tests__/SelectInput.spec.js
+++ src/components/__tests__/SelectInput.spec.js
@@ -1,6 +1,5 @@
 import { mount } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
-import { nextTick } from "vue";
 import SelectInput from "../SelectInput.vue";
 
 describe("SelectInput", () => {
