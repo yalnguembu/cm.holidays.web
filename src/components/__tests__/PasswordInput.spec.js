01cb75a3168c68f40c69620d0e217e8db66a71a2
Yal Nguembu <yalnguembu@gmail.com>
2023-01-27 14:14:39 +0100

    Update PasswordInput.spec.js
    remove skipped test

--- src/components/__tests__/PasswordInput.spec.js
+++ src/components/__tests__/PasswordInput.spec.js
@@ -2,7 +2,7 @@ import { mount } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
 import PasswordInput from "../PasswordInput.vue";
 
-describe.skip("PasswordInput", () => {
+describe("PasswordInput", () => {
   const wrapper = mount(PasswordInput, {
     props: {
       label: "Password",
