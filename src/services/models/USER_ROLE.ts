17aadacaec1ca3523ee010b4693d63bfddb5f71f
yalnguembu <yalnguembu@gmail.com>
2023-10-18 23:29:55 +0100

    -fix: remove generated folder to the project repository

--- src/services/models/USER_ROLE.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export enum USER_ROLE {
-    ADMIN = 'ADMIN',
-    EMPLOYEE = 'EMPLOYEE',
-    HUMAN_RESOURCE = 'HUMAN_RESOURCE',
-}
