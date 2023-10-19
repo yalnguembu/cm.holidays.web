d92d5a6fb975d2b8cdf1eb625066417a6dd744dd
WAFFEU IBR <73952440+WIBRC45920@users.noreply.github.com>
2023-10-19 15:00:31 +0100

    working/hldy-15 | fix: Resolve the build problem in production (#42)
    Co-authored-by: rayn.waffeu <rayn.waffeu@lao-sarl.cm>

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  test: {
-    environment: 'happy-dom'
-  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
