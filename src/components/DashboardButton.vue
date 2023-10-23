31fcfc9c39a0bbc8d5c2ca79ae06756ec82f19cb
Yal Nguembu <yalnguembu@gmail.com>
2023-10-23 17:43:57 +0100

    merge develop into main (#54)
    * update holiday creation component e2e test
    
    * refactoring e2e test
    
    * holiday-2 -test:components
    
    * feat:build service and post crud
    
    * hldy-5 -feat:display users list
    
    * hldy-6 -feat:create user (#32)
    
    * Working/hldy 4 (#33)
    
    
    * HLDY-4 -refactor: improve a greet description to the project*
    
    * -fix: remove generated folder to the project repository (#34)
    
    * hldy-8 -refactor: improve handelRequest method to implement some clean code convention (#35)
    
    * task: configure redirection  netlify deployment server (#36)
    
    * hldy-11 -task: implement api url configuration (#37)
    
     -task: implement api url configuration
    
    * release-0.2 (#38)
    
    * Hldy 12 (#40)
    
    * release-0.2
    
    * -task: upgrade typescript version
    
    * -task: upgrade typescript version
    
    * -task: upgrade typescript version (#41)
    
     -task: upgrade typescript version
    
    * working/hldy-15 | fix: Resolve the build problem in production (#42)
    
    Co-authored-by: rayn.waffeu <rayn.waffeu@lao-sarl.cm>
    
    * HLDY-20 -fix: Token is not inserted in the request header after the login (#45)
    
    * HLDY-21 -fix: change the favicon (#46)
    
    Co-authored-by: nguembu.fotso <nguembu.fotso@lao-sarl.cm>
    
    * HLDY-22 -enhance: add the enterprise logo in the signin page and the navbar (#47)
    
    * release 0.3 (22-10-2023)
    
    * undo merge main into develop (#53)
    
    ---------
    
    Co-authored-by: WAFFEU IBR <73952440+WIBRC45920@users.noreply.github.com>
    Co-authored-by: rayn.waffeu <rayn.waffeu@lao-sarl.cm>
    Co-authored-by: nguembu.fotso <nguembu.fotso@lao-sarl.cm>

---
+++ src/components/DashboardButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <RouterLink
+      :to="path"
+      :title="title"
+      class="p-8 rounded-lg border flex flex-col items-center justify-center hover:shadow-lg hover:shadow-blue-200 inline-block md:h-72 text-gray-500 hover:text-blue-primary"
+  >
+    <component
+        :is="icon"
+        class="mr-4 h-[40px] stroke-2 stoke-gray-500"
+    />
+    <br />
+    <span class="text-xl font-bold mt-8">{{ label }}</span>
+  </RouterLink>
+</template>
+<script setup lang="ts">
+defineProps({
+  path:{
+    type: String,
+    required: true
+  },
+  label:{
+    type: String,
+    required: true
+  },
+  title:{
+    type: String,
+  },
+  icon: {
+    type: Object,
+    required: true
+  }
+})
+</script>
