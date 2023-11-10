2427d3cbeb6b0d4f12c5ea9f6d7ed282cd43a396
WIBRC45920 <waffeu12092001@gmail.com>
2023-11-10 10:25:06 +0100

    HOLIDAY-12 | [ADMIN] | get all roles

--- src/views/admin/RolesList.vue
+++ src/views/admin/RolesList.vue
@@ -7,44 +7,60 @@
     />
     <HeaderComponent
         title="Roles"
-        description="List of roles within the system"
+        description="List of roles within the system with their status"
     >
       <CreationButton
           title="New Role"
           @click.stop="toggleShouldDisPlayCreationModal"
       />
     </HeaderComponent>
-    <section data-test="roles-list" class="h-full w-full flex flex-col justify-center items-center min-h-[70vh]">
-      <OpenFolderIcon class="stroke-gray-500/10 h-[20rem]" />
-      <p class="text-xl text-gray-500 text-center 2xl:w-1/4"> No roles has already been created! Please click on the button up there to create a new one. </p>
+    <section data-test="roles-list" class="min-h-[70vh] relative">
+      <div v-if="isRoleListLoading" class="absolute top-1/2 left-[45%] -translate-x-1/2 -translate-y-1/2">
+        <SpinnerLoader class="h-14 w-14 " />
+      </div>
+      <template v-else>
+        <div v-if="roles.length" class="flex flex-wrap gap-x-2.5 mt-6">
+          <div
+              v-for="role in roles" :key="role.id"
+              class="border p-6 rounded-lg h-42 overflow-hidden hover:bg-gray-100/30 hover:shadow-lg relative">
+            <h4 class="text-blue-900 font-semibold text-2xl mb-4 flex gap-x-2">
+              <span>{{ role.type.split('_').join(" ") }}</span>
+              <StatutBadge :is-active="role.isActive" />
+            </h4>
+            <p class="text-gray-500 text-sm line-clamp-3">{{role.description}}</p>
+          </div>
+        </div>
+        <div v-else class="flex flex-col justify-center items-center">
+          <OpenFolderIcon class="stroke-gray-500/10 h-[20rem]" />
+          <p class="text-xl text-gray-500 text-center 2xl:w-1/4">
+            No roles has already been created! Please click on the button up there to create a new one.
+          </p>
+        </div>
+      </template>
     </section>
   </div>
 </template>
 <script setup lang="ts">
-import {onBeforeMount, ref} from "vue";
+import {onBeforeMount, Ref, ref} from "vue";
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import CreationButton from "@/components/CreationButton.vue";
 import RoleCreationModal from "@/components/modals/RoleCreationModal.vue";
 import {Role} from "@/domain/Role";
 import {useRoleStore} from "@/store/role";
 import OpenFolderIcon from "@/components/icons/OpenFolderIcon.vue";
+import SpinnerLoader from "@/components/SpinnerLoader.vue";
+import StatutBadge from "@/components/badges/StatutBadge.vue";
 
 const shouldDisplayCreationModal = ref<boolean>(false);
 const toggleShouldDisPlayCreationModal = () =>
     (shouldDisplayCreationModal.value = !shouldDisplayCreationModal.value);
 
-const shouldDisplayDetailsModal = ref<boolean>(false);
-const toggleShouldDisPlayDetailsModal = () =>
-    (shouldDisplayDetailsModal.value = !shouldDisplayDetailsModal.value);
-
-const showDetails = (id: string) => {
-  toggleShouldDisPlayDetailsModal();
-};
 const isRoleListLoading = ref<boolean>(false)
-const roles = ref<Role[]>([])
-const fetchRoles = async () => {
+const roles = ref<Role[]>([]) as Ref<Role[]>;
+const fetchRoles = async (): Promise<void> => {
   isRoleListLoading.value = true;
-  roles.value = await useRoleStore().getAllRoles();
+  const rolesResponse = await useRoleStore().getAllRoles();
+  roles.value = rolesResponse.data as Role[];
   isRoleListLoading.value = false;
 }
 
